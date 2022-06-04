1706c4e7b1bc2963e498f6c7c6367e273120b59c
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-04 15:45:46 +0300

    learn protorype

--- js/scrypt.js
+++ js/scrypt.js
@@ -4,6 +4,37 @@ const inputTest = document.querySelector(".test__input");
 const buttonTest = document.querySelector(".test__button");
 const resultTest = document.querySelector(".test__result");
 
+// const person = {
+//     name: 'Никита',
+//     age: 22,
+//     city: 'Минск',
+//     greet: function () {
+//         console.log(`Привет, меня зовут ${this.name}! Мне ${this.age}. Я родом из города ${this.city}`)
+//     }
+// }
+
+const person = new Object({
+    name: 'Никита',
+    age: 22,
+    city: 'Минск',
+    greet: function () {
+        console.log(`Привет, меня зовут ${this.name}! Мне ${this.age}. Я родом из города ${this.city}`)
+    }
+});
+
+Object.prototype.sayHello = function () {
+    console.log('Hello.')
+}
+
+
+const lena = Object.create(person);
+lena.name = 'Елена';
+
+const str = new String('YoYoYO');
+
+
+
+
 /*--------
 buttonTest.addEventListener('click', function () {
     window.location.href = '/about.html';
@@ -25,18 +56,27 @@ setTimeout(() => {
 }, 1000);
 ---------*/
 
-
 // const myName = 'Nikita';
 // localStorage.removeItem('nikita');
 // localStorage.setItem('name', myName);
 // console.log(localStorage.getItem('name'));
 
+/*------
 const object = {
     name: 'Nikita',
     age: 22
 }
 //localStorage.setItem('person', JSON.stringify(object));
 const raw = localStorage.getItem('person');
-//raw.name = 'Oleg'
+const person = JSON.parse(raw)
+person.name = 'Oleg'
 
-console.log(JSON.parse(raw))
+// console.log(raw);
+// console.log(person)
+
+window.addEventListener('storage', event => {
+    console.log(event)
+});
+
+window.onstorage = () => { };
+-------*/
